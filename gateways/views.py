f4e70749ce1e995eabc44308919491204ca948fe
piyushkat <piyush.kathuria@technogetic.com>
2023-02-28 18:26:56 +0530

    create put and delete functionality on this function

--- gateways/views.py
+++ gateways/views.py
@@ -1,12 +1,9 @@
-from rest_framework import views
+import requests
+from rest_framework.decorators import csrf_exempt
 from rest_framework.response import Response
-import requests
-from rest_framework.generics import GenericAPIView
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 
-from django.shortcuts import render
-from django.http import JsonResponse
-import requests
-from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def api_handler(request, service_name, endpoint):
@@ -15,18 +12,35 @@ def api_handler(request, service_name, endpoint):
         base_url = 'http://127.0.0.1:8001/api/auth/'
     elif service_name == 'service2':
         base_url = 'http://127.0.0.1:8002/api/product/'
-    else:   
-        return JsonResponse({'error': 'Invalid service name'})
-    
+    else:
+        return Response({'error': 'Invalid service name'}, status=HTTP_400_BAD_REQUEST)
+
     if request.method == 'GET':
         # Make a GET request to the microservice
         response = requests.get(base_url + endpoint)
-        return JsonResponse(response.json(), status=response.status_code)
+        return Response(response.json(), status=response.status_code)
+    
     elif request.method == 'POST':
         # Make a POST request to the microservice
-        data = request.body
+        data = request.data
         headers = {'Content-Type': 'application/json'}
         response = requests.post(base_url + endpoint, data=data, headers=headers)
-        return JsonResponse(response.json(), status=response.status_code)
+        return Response(response.json(), status=response.status_code)
+    
+    elif request.method == 'PUT':
+        # Make a PUT request to the microservice
+        data = request.data
+        headers = {'Content-Type': 'application/json'}
+        response = requests.put(base_url + endpoint, data=data, headers=headers)
+        return Response(response.json(), status=response.status_code)
+    
+    elif request.method == 'DELETE':
+        # Make a DELETE request to the microservice
+        response = requests.delete(base_url + endpoint)
+        return Response(status=response.status_code)
+    
+    else:
+        return Response({'error': 'Unsupported method'}, status=HTTP_400_BAD_REQUEST)
+
 
 
